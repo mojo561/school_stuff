4bf0af29cdbd537b08b04658df3dde5f50ff7faa
mojo <josh.monague@sheridanc.on.ca>
2017-03-30 10:53:26 -0400

    freq char counter commit (touch up)

--- assembly_stuff/freq_counter/fc.asm
+++ assembly_stuff/freq_counter/fc.asm
@@ -1,15 +1,6 @@
 ;RDI, RSI, RDX, RCX, R8, R9
 ;read a string from the console
 ;for each letter in the alphabet, print a frequency count of the number of times that each letter was present (case insensitive) in the input string
-;ASCII 65 = A, 97 = a.
-;need int array a with 26 elements (for each letter in the alphabet)
-;loop through the input string
-;  check if char c at index i is in the alphabet (65 <= c <= 90 OR 97 <= c <= 122)
-;  Mod each char by 65 and 97. check if remainder is < 26
-;  if remainder r < 26, ++a[r]
-;loop through a
-;  if a[i] != 0
-;    print "a[i]+65: a[i]"
 global main
 	extern	fgets
 	extern	puts
@@ -28,15 +19,15 @@ main:
 	mov	rdx, [stdin]	;can't just mov 0 into rdx, need extern address of stdin
 	call	fgets
 	xor	rcx, rcx
-getlen:
+getlen:				;calculate length of buffer
 	xor	rdi, rdi
-	mov	dil, [buffer + rcx]
+	mov	dil, byte [buffer + rcx]
 	cmp	rdi, 0x0
 	je	nelteg
 	inc	rcx
 	jmp	getlen
-nelteg:
-	mov	[strlen], rcx
+nelteg:				;
+	mov	[strlen], rcx	;whatever is in rcx is our string length
 	xor	rcx, rcx
 getfrq:
 	cmp	rcx, [strlen]
@@ -61,7 +52,6 @@ lower:
 	cmp	rax, 0	;is the quotient 0? (if it is, character is too low to be alphabetic)
 	je	next
 	inc	qword [frqarr + rdx*8]	;frqarr is an array of qwords (8 bytes). update frqarr at index [rdx]
-	jmp	next
 next:
 	inc	rcx
 	jmp	getfrq
@@ -71,16 +61,16 @@ frqloop:
 	cmp	rcx, 26
 	je	poolqrf
 	mov	rdx, qword [frqarr + rcx*8]
-	cmp	rdx, 0
-	je	skip
+	cmp	rdx, 0		;
+	je	skip		;skip array index if it's value is 0
 	mov	rdi, fmt
-	mov	rsi, 65
-	add	rsi, rcx
+	mov	rsi, 65		;ASCII 'A'
+	add	rsi, rcx	;add a value to 'A' to get a new alphabet char
 	call	printf
 skip:
-	mov	rcx, [i]
-	inc	rcx
-	mov	[i], rcx
+	mov	rcx, [i]	;have to update counter because call to printf destroys rcx
+	inc	rcx		;
+	mov	[i], rcx	;
 	jmp	frqloop
 poolqrf:
 	mov	rax, 0		;epilog
